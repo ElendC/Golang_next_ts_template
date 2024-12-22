121ca006680a7a73c4adc523eefe36828407513f
Elend Egid <250058@uis.no>
2024-12-22 12:51:32 +0100

    fixed links to auth and home

---
+++ Notes/TODO.md
@@ -0,0 +1,2 @@
+1. Add CORS
+2. Check History API: history.pushState() and history.replaceState()
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function NavBar(){
     return(
@@ -18,21 +19,25 @@ export default function NavBar(){
             <div className="flex justify-between w-full h-full text-navBarText">
                 {/*nav links Left side*/}
                 <ul className="flex items-center">
-                    <li className="pl-3 hover:text-navBarHover cursor-pointer h-4/5 min-w-12">
-                        <img className="h-full drop-shadow-2xl"
-                             src="/icons/UiS_Logo.svg" alt="Logo"/>
+                    <li className="pl-3 hover:text-navBarHover  h-4/5 min-w-12">
+                        <Link href="/">
+                            <img className="h-full drop-shadow-2xl cursor-pointer"
+                                 src="/icons/UiS_Logo.svg" alt="Logo"/>
+                        </Link>
                     </li>
                 </ul>
                 {/*nav links Right side*/}
                 <ul className="flex flex-wrap items-center overflow-hidden  max-h-14">
                     <li><i className="m-2 hover:text-navBarHover cursor-pointer">Search</i></li>
-                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Home</i></li>
-                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">About</i></li>
-                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Something</i></li>
-                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Something</i></li>
+                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Stud. Org</i></li>
+                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Nyheter</i></li>
+                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">SIS</i></li>
+                    <li><i className="m-2 hover:text-navBarHover cursor-pointer">Tilbud</i></li>
                     <li>
-                        <img className="h-full drop-shadow-2xl m-3 hover:text-navBarHover cursor-pointer"
-                             src="/icons/log-in.svg" alt="Logo"/>
+                        <Link href={"/authentication"}>
+                            <img className="h-full drop-shadow-2xl m-3 hover:text-navBarHover cursor-pointer"
+                                 src="/icons/log-in.svg" alt="Logo"/>
+                        </Link>
                     </li>
                 </ul>
             </div>

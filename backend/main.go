b2255e4111809409bc23139154569e5682c11cb3
Elend Egid <250058@uis.no>
2025-01-01 13:10:44 +0100

    added todo

--- backend/main.go
+++ backend/main.go
@@ -4,17 +4,18 @@ import (
 	"backend/database"
 	"backend/environment"
 	"backend/routes"
+	"log"
 )
 
 func main() {
 	err := environment.LoadEnvFile("./environment/.env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
-	}
+	} //TODO: make a print instead of log
 
 	// Initialize and test Database Connection
 	database.DbConfig()
-	
+
 	// Handlers
 	routes.SetupRoutes()
 
